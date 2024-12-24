148e7aba2cb73f86bee91373dd4155d092032688
danilkompaniets <kompaniets1592925@gmail.com>
2024-12-25 00:13:12 +0300

    canvas layout added

---
+++ app/board/[boardId]/_components/canvas.tsx
@@ -0,0 +1,20 @@
+"use client"
+
+import {Info} from "@/app/board/[boardId]/_components/info";
+import {Participants} from "@/app/board/[boardId]/_components/participants";
+import {Toolbar} from "@/app/board/[boardId]/_components/toolbar";
+
+interface CanvasProps {
+    boardId: string
+}
+
+export const Canvas = ({boardId}: CanvasProps) => {
+    return (
+        <main
+            className={"h-full w-full relative bg-neutral-50 touch-none"}>
+            <Info/>
+            <Participants/>
+            <Toolbar/>
+        </main>
+    )
+}
---
+++ app/board/[boardId]/page.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import {Canvas} from "@/app/board/[boardId]/_components/canvas";
+
+interface BoardIdPageProps {
+    params: {
+        boardId: string
+    }
+}
+
+const BoardIdPage = ({
+                         params,
+                     }: BoardIdPageProps) => {
+    return (
+        <Canvas boardId={params.boardId} />
+    )
+}
+
+export default BoardIdPage;
