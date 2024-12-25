a28294dd305d69782e75dad1f01a84120ef8247a
danilkompaniets <kompaniets1592925@gmail.com>
2024-12-25 17:11:52 +0300

    room participants added

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,18 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+const colors = [
+  "#007BFF",
+  "#6C757D",
+  "#28A745",
+  "#DC3545",
+  "#FFC107",
+  "#17A2B8",
+  "#343A40",
+];
+
+
+export const connectionIdToColor = (connectionId: number): string => {
+  return colors[connectionId % colors.length]
+}
